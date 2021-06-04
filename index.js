ae10dac5fc4ff3bbac807b1ba825e0378ed2bd8b
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2021-06-04 16:40:54 -0400

    dynamic Readme with weather and instagram data, and auto generated via workflow

---
+++ index.js
@@ -0,0 +1,75 @@
+require('dotenv').config();
+const Mustache = require('mustache');
+const fetch = require('node-fetch');
+const fs = require('fs');
+const puppeteerService = require('./services/puppeteer.service')
+
+const MUSTACHE_MAIN_DIR = './main.mustache';
+
+/**
+ * DATA is the object that contains all
+ * the data to be provided to Mustache
+ * Notice the "name" and "date" property.
+ */
+let DATA = {
+  name: 'Seth',
+  date: new Date().toLocaleDateString('en-US', {
+    weekday: 'long',
+    month: 'long',
+    day: 'numeric',
+    hour: 'numeric',
+    minute: 'numeric',
+    timeZoneName: 'short',
+  }),
+};
+
+async function setWeatherInfo() {
+  await fetch(
+    `https://api.openweathermap.org/data/2.5/weather?id=4498231&appid=${process.env.OPEN_WEATHER_MAP_KEY}&units=metric`
+  )
+    .then((r) => r.json())
+    .then((r) => {
+      DATA.city_temperature = Math.round(r.main.temp);
+      DATA.city_weather = r.weather[0].description;
+      DATA.city_weather_icon = r.weather[0].icon;
+      DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+      DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-US', {
+        hour: '2-digit',
+        minute: '2-digit',
+      });
+    });
+}
+
+async function setInstagramPosts() {
+  const instagramImages =
+    await puppeteerService.getLatestInstagramPostsFromAccount('visitnc', 3);
+  DATA.img1 = instagramImages[0];
+  DATA.img2 = instagramImages[1];
+  DATA.img3 = instagramImages[2];
+}
+
+/**
+ * A - We open 'main.mustache'
+ * B - We ask Mustache to render our file with the data
+ * C - We create a README.md file with the generated output
+ */
+function generateReadMe() {
+  fs.readFile(MUSTACHE_MAIN_DIR, (err, data) => {
+    if (err) throw err;
+    const output = Mustache.render(data.toString(), DATA);
+    fs.writeFileSync('README.md', output);
+  });
+}
+
+async function action() {
+  await setWeatherInfo();
+  await setInstagramPosts();
+  await generateReadMe();
+  await puppeteerService.close();
+}
+
+action();
+
