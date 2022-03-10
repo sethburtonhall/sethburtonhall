34d58688914d81ac58adb599b9e79b17cd74683d
Seth Hall <1033332+sethburtonhall@users.noreply.github.com>
2022-03-10 15:53:52 -0500

    working with time zone issue

--- index.js
+++ index.js
@@ -33,21 +33,13 @@ async function setWeatherInfo() {
       DATA.city_temperature = Math.round(r.main.temp);
       DATA.city_weather = r.weather[0].description;
       DATA.city_weather_icon = r.weather[0].icon;
-      // DATA.sun_rise = new Date(r.sys.sunrise * 1000).toLocaleString('en-US', {
-      //   hour: '2-digit',
-      //   minute: '2-digit',
-      // });
-      // DATA.sun_set = new Date(r.sys.sunset * 1000).toLocaleString('en-US', {
-      //   hour: '2-digit',
-      //   minute: '2-digit',
-      // });
-      DATA.sun_rise = new Date(r.sys.sunrise).toLocaleDateString('en-US', {
+      DATA.sun_rise = new Date(r.sys.sunrise).toLocaleString('en-US', {
         hour: 'numeric',
         minute: 'numeric',
         timeZoneName: 'short',
         timeZone: 'America/New_York',
       });
-      DATA.sun_set = new Date(r.sys.sunset).toLocaleDateString('en-US', {
+      DATA.sun_set = new Date(r.sys.sunset).toLocaleString('en-US', {
         hour: 'numeric',
         minute: 'numeric',
         timeZoneName: 'short',
